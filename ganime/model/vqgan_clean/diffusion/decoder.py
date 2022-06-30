e4abbb52cdc67bb3c6ba8d37fbe211b25df73d0b
farid.abdalla <farid.abdalla@he-arc.ch>
2022-06-30 07:05:39 +0200

    Correction of some problems with the VQ-GAN (added sigmoid)

--- ganime/model/vqgan_clean/diffusion/decoder.py
+++ ganime/model/vqgan_clean/diffusion/decoder.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Model, layers
 from tensorflow_addons.layers import GroupNormalization
-from tensorflow.keras.layers import BatchNormalization
 
 from .layers import AttentionBlock, ResnetBlock, Upsample
 
@@ -96,7 +95,7 @@ class Decoder(layers.Layer):
             # activation="sigmoid",
             padding="same",
         )
-        # self.outputs = layers.Activation("sigmoid", dtype="float32", name="predictions")
+        self.outputs = layers.Activation("sigmoid", dtype="float32", name="predictions")
 
     def call(self, inputs, training=True, mask=None):
 
@@ -114,5 +113,5 @@ class Decoder(layers.Layer):
         h = self.norm_out(h)
         h = keras.activations.swish(h)
         h = self.conv_out(h)
-        # h = self.outputs(h)
+        h = self.outputs(h)
         return h
