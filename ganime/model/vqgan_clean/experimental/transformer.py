7264c28d1897123b3db5992c6a6fc68da6443ebf
farid.abdalla <farid.abdalla.13@gmail.com>
2022-07-18 11:12:41 +0900

    Add possibility to change how the remaining number of frames is given via the config file

--- ganime/model/vqgan_clean/experimental/transformer.py
+++ ganime/model/vqgan_clean/experimental/transformer.py
@@ -1,38 +1,89 @@
 from tensorflow.keras import layers
 from tensorflow.keras import Model
-from transformers import TFGPT2Model, TFGPT2LMHeadModel, GPT2Config
 import tensorflow as tf
+from transformers import TFPreTrainedModel
 
 
 class Transformer(Model):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.transformer = TFGPT2Model.from_pretrained("gpt2-medium")
+        self.remaining_frames_method = self.get_remaining_frames_method(config)
+        self.transformer = self.load_transformer(self.remaining_frames_method)
+
+    def get_remaining_frames_method(self, config) -> str:
+        """Get the method to use for remaining frames.
+        Check if the method is set inside the configuration, otherwise use concat as the default.
+        """
+        if "remaining_frames_method" in config:
+            return config["remaining_frames_method"]
+        else:
+            return "concat"
+
+    def load_transformer(self, method) -> TFPreTrainedModel:
+        print("using method ", method)
+        if method == "own_embeddings":
+            from ganime.model.vqgan_clean.experimental.gpt2_embedding import TFGPT2Model
+
+            transformer = TFGPT2Model.from_pretrained("gpt2-medium")
+
+        else:
+            from transformers import TFGPT2Model
+
+            transformer = TFGPT2Model.from_pretrained("gpt2-medium")
+        return transformer
+
+    def concatenate_inputs(
+        self, remaining_frames, last_frame_indices, previous_frame_indices
+    ) -> tf.Tensor:
+        if self.remaining_frames_method == "concat":
+            return tf.concat(
+                [remaining_frames, last_frame_indices, previous_frame_indices], axis=1
+            )
+        else:
+            return tf.concat([last_frame_indices, previous_frame_indices], axis=1)
+
+    def call_transformer(
+        self, transformer_input, remaining_frames, training, attention_mask
+    ):
+        if self.remaining_frames_method == "concat":
+            return self.transformer(
+                transformer_input, training=training, attention_mask=attention_mask
+            )
+        elif self.remaining_frames_method == "token_type_ids":
+            return self.transformer(
+                transformer_input,
+                token_type_ids=remaining_frames,
+                training=training,
+                attention_mask=attention_mask,
+            )
+        elif self.remaining_frames_method == "own_embeddings":
+            return self.transformer(
+                transformer_input,
+                remaining_frames_ids=remaining_frames,
+                training=training,
+                attention_mask=attention_mask,
+            )
+        else:
+            raise ValueError(
+                f"Unknown remaining_frames_method {self.remaining_frames_method}"
+            )
 
     def call(self, inputs, training=True, mask=None):
         remaining_frames, last_frame_indices, previous_frame_indices = inputs
-        # last_frame_indices = tf.expand_dims(last_frame_indices, axis=1)
-        # previous_frame_indices = tf.expand_dims(previous_frame_indices, axis=1)
-        # tf.print("last_frame_indices", tf.shape(last_frame_indices))
-        # tf.print("previous_frame_indices", tf.shape(previous_frame_indices))
         remaining_frames = tf.expand_dims(remaining_frames, axis=1)
         shape_to_keep = tf.shape(last_frame_indices)[1]
-        h = tf.concat([last_frame_indices, previous_frame_indices], axis=1)
+
+        h = self.concatenate_inputs(
+            remaining_frames, last_frame_indices, previous_frame_indices
+        )
 
         transformer_input = h[:, :-1]
         mask = tf.ones_like(transformer_input) * tf.cast(
             tf.cast(remaining_frames, dtype=tf.bool), dtype=remaining_frames.dtype
         )
 
-        h = self.transformer(
-            transformer_input,
-            remaining_frames_ids=remaining_frames,
-            training=training,
-            attention_mask=mask,
-        )
-        # h = h.logits
+        h = self.call_transformer(transformer_input, remaining_frames, training, mask)
         h = h.last_hidden_state
         h = h[:, -shape_to_keep:]
-        # tf.print("h", tf.shape(h))
         return h
